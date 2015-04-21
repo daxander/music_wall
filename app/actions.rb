247c3fc2681547b1d2ae9fcaeb104611ab169dfe
David Feimer <davidfeimer@gmail.com>
2015-04-21 00:43:57 +0000

    Functional App b4 Styling

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,33 @@
 get '/' do
   erb :index
 end
+
+
+get '/songs' do
+  @songs = Song.all
+  erb :'songs/index'
+end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
+end
+
+post '/songs' do
+  @song = Song.new(
+    url: params[:url],
+    title: params[:title],
+    author: params[:author]
+    )
+    if @song.save
+      redirect '/songs'
+    else
+      erb :'songs/new'
+  end
+end
+
+
